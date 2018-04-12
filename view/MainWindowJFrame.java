aa346d702fe4ff65e5110c5539aa1c9dd4037dfb
Juan Buendia <juan.buendia@uptc.edu.co>
2018-04-12 14:16:29 -0500

    Se creo el dialogo para leer la carpeta, el controlador y el fileManager

--- view/MainWindowJFrame.java
+++ view/MainWindowJFrame.java
@@ -1,6 +1,17 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 
 public class MainWindowJFrame extends JFrame{
 
@@ -11,6 +22,26 @@ public class MainWindowJFrame extends JFrame{
 		setTitle(TITLE);
 		setExtendedState(MAXIMIZED_BOTH);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		JLabel lbImg = new JLabel("IMAGE");
+		lbImg.setSize(500,200);
+		lbImg.setBorder(BorderFactory.createTitledBorder("Image:"));
+		add(new JScrollPane(lbImg));
+		
+		JButton btnSelectImage = new JButton("Select image");
+		btnSelectImage.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnSelectImage.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser jf = new JFileChooser();
+				int option = jf.showOpenDialog(getContentPane());
+				if (option == JFileChooser.APPROVE_OPTION) {
+					lbImg.setIcon(new ImageIcon(jf.getSelectedFile().getPath()));
+				}
+			}
+		});
+		add(btnSelectImage, BorderLayout.PAGE_END);
 		
 		setVisible(true);
 	}
